c65600894f3af61112d64399f24798f24cd81a7c
trisha mcd <trisha.mcdougall@my.jcu.edu.au>
2025-10-28 09:18:52 +1000

    Complete state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,7 +1,7 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
+File needs reformatting - using exceptions
 """
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
