195babe6c9e231718ca1ef9f46ddb172fc36d10b
trisha mcd <trisha.mcdougall@my.jcu.edu.au>
2025-10-30 14:19:46 +1000

    Complete guitar class exercise

---
+++ prac_06/guitar.py
@@ -0,0 +1,41 @@
+"""CP1404 - Practical
+Creating a class for guitars
+Started - Finished:
+Estimated time: 50 minutes
+Actual time:
+ """
+YEAR = 2022
+
+
+class Guitar:
+    """Represent Guitar"""
+
+    def __init__(self, name="", year=0, cost=0.0):
+        """Initialise guitar values"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return values into string format"""
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def get_age(self):
+        """Returns how old the guitar is in years"""
+        age = YEAR - self.year
+        return age
+
+    def is_vintage(self):
+        """Returns True if the guitar is 50 or more years old"""
+        if self.get_age() >= 50:
+            return True
+        else:
+            return False
+
+
+
+gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another_guitar = Guitar("Another Guitar", 2013, 16035.40)
+print(gibson)
+print(gibson.get_age())
+print(gibson.is_vintage())
