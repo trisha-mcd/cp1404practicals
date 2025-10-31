bad4d09b3817e9e691776b9f55235a0beb5e2008
trisha mcd <trisha.mcdougall@my.jcu.edu.au>
2025-10-31 10:56:05 +1000

    Complete guitar exercise

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,8 +1,8 @@
 """CP1404 - Practical
 Creating a class for guitars
-Started - Finished: 2:27pm - 4:00pm
+Started - Finished: 2:27pm - 2:48pm
 Estimated time: 40mins
-Actual time: 2 hrs
+Actual time: 20mins
  """
 from prac_06.guitar import Guitar
 
@@ -10,32 +10,12 @@ from prac_06.guitar import Guitar
 def main():
     """Use a list to store all user's guitars. Keep inputting until blank name then print all details"""
 
-    # # testing
-    # gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    # another_guitar = Guitar("Another Guitar", 2013)
-    # print(f"{gibson.name} get_age() - Expected 100. Got {gibson.get_age()}")
-    # print(f"{another_guitar.name} get_age() - Expected 9. Got {another_guitar.get_age()}")
-    # print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
-    # print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
-
-    guitars = []
-    print("My guitars!")
-    is_valid_input = False
-    while not is_valid_input:
-        try:
-            name = input("Name: ").strip()
-            if name == "":
-                is_valid_input = True  # stop loop
-                continue
-            year = int(input("Year: "))
-            cost = float(input("Cost: "))
-            guitars.append(Guitar(name, year, cost))
-            print(f"{name} ({year}) : ${cost} added.\n")
-        except ValueError:
-            print("Invalid")
-    print("... snip ...\n")
-    print("These are my guitars")
-    for i, guitar in enumerate (guitars):
-        print(f"Guitar {i}: {guitar}")
+    # testing
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another_guitar = Guitar("Another Guitar", 2013)
+    print(f"{gibson.name} get_age() - Expected 100. Got {gibson.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected 9. Got {another_guitar.get_age()}")
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
 
 main()
